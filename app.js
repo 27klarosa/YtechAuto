6b28dd477cee5bd2c1ec6b369c2e49581f5d58ec
Kayden <27KlaRosa@live.ytech.edu>
2026-03-17 14:50:32 -0400

    wegrgrrerererere

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const sqlite3 = require('sqlite3');
 const fs = require('fs');
 const ejs = require('ejs');
 const node = require('node')
+const session = require('express-session');
 
-const app = express(); 
-//for use with node.js
+const app = express();
 
+const db = new sqlite3.Database('database/database.sql', (err) => {
+    if (err) return console.error('Error connecting to database:', err.message);
+}); 
+
+const 
