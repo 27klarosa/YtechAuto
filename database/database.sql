b88a7d067c5d4f9369a61aa7b00e045940a5020b
Kayden <27KlaRosa@live.ytech.edu>
2026-04-08 11:18:12 -0400

    vkifnjbmn f

--- database/database.sql
+++ database/database.sql
@@ -90,12 +90,6 @@ CREATE TABLE emissions (
     ticketID INTEGER NOT NULL,
     item TEXT NOT NULL,
     input TEXT NOT NULL
-);
-
-CREATE TABLE signitures (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ticketID INTEGER NOT NULL,
-
 )
 
 -- "uploads/ filenamefromdb" + ".png"
